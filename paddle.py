e6b0a1ef2b7b35b3138e9ab82214b05c05583d6e
natertater23 <angelbaseball2@gmail.com>
2019-09-18 19:18:10 -0700

    created paddle class and

---
+++ paddle.py
@@ -0,0 +1,37 @@
+import pygame
+RED = (255, 0, 0)
+
+
+class Paddle(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super.__init__()
+        # create the surface, we make it transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(RED)
+        self.image.set_colorkey(RED)
+        # draw rect with given color and width height, coords dont matter since will be moving anyways
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        # make that rect our rect
+        self.rect = self.image.get_rect()
+    # These will affect the way the paddle moves and to make sure they stay in scope
+    def move_up(self, pixels):
+
+        self.rect.y -= pixels
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+    def move_down(self, pixels):
+        self.rect.y += pixels
+        if self.rect.y > 400:
+            self.rect.y = 400
+
+    def move_left(self, pixels):
+        self.rect.x -= pixels
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+    def move_right(self, pixels):
+        self.rect.x += pixels
+        if self.rect.x > 700:
+            self.rect.x = 700
--- pong_pyg_main.py
+++ pong_pyg_main.py
@@ -1,9 +1,45 @@
 import pygame
 pygame.init()
-
+# Colors to be used for board and sphere
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
 size = (800, 500)
-screen = pygame.display
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Pong")
+# This will keep the game loop running until the game is over
+gameLoop = True
+# Set to 60 later
+clock = pygame.time.Clock()
+
+def draw_dashed_line(surface, color, start_pos, end_pos, width = 2, length = 10):
+    # for loop to draw small lines to form a bigger dashed line
+    pygame.draw.line(surface, color, start_pos, end_pos, width)
+
+
+
+while gameLoop:
+    # user does something
+        # if close
+            # close
+
+    # Draw necessary items
+    screen.fill(RED)
+    # Draw line here
+
+    # update
+    pygame.display.flip()
+    # 60 FPS
+    clock.tick(60)
+
+
+
+
+
+pygame.quit()
+
+
+
+
+
 
