2569823e7424d7bfeafb7a157bb6ec1c60c8bd7e
natertater23 <angelbaseball2@gmail.com>
2019-09-19 22:01:56 -0700

    fixed cpu

--- ball.py
+++ ball.py
@@ -21,4 +21,4 @@ class Ball(pygame.sprite.Sprite):
 
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
-        self.velocity[1] = randint(-8, 8)
+        self.velocity[1] = randint(-4, 4)
