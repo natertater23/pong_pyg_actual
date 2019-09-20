db608b34556403c843cbf51aeb9239fa188a73d9
natertater23 <angelbaseball2@gmail.com>
2019-09-19 23:12:29 -0700

    added sounds and play again feature

--- ball.py
+++ ball.py
@@ -21,4 +21,4 @@ class Ball(pygame.sprite.Sprite):
 
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
-        self.velocity[1] = randint(-4, 4)
+        self.velocity[1] = -self.velocity[1]
